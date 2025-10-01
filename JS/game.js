1d19225a8d123de95c9a98ee157d370a0674c29f
Ironjuan <juanjosearevalgaona@gmail.com>
2025-10-01 15:28:39 -0500

    End product

--- JS/game.js
+++ JS/game.js
@@ -15,11 +15,17 @@ async function cargarJuego(appId) {
     if (true) {
         console.log("Cargando juego:");
 
-        contenedor.querySelector('.title').textContent = game.name;
-        contenedor.querySelector('.description').textContent = game.short_description;
-        contenedor.querySelector('.thumbnail').src = game.capsule_image;
+        contenedor.querySelector('.background-image').src = game.background;
+
+        contenedor.querySelector('.title').innerHTML += game.name;
+        contenedor.querySelector('.short_description').innerHTML += game.short_description;
+        contenedor.querySelector('.thumbnail').src = game.header_image;
         contenedor.querySelector('.thumbnail').alt = game.name;
-        contenedor.querySelector('.price').textContent = game.is_free ? "Gratis" : `$${game.price_overview.final / 100}`;
+        contenedor.querySelector('.price').innerHTML += game.is_free ? "Gratis" : `$${game.price_overview.final / 100}`;
+        contenedor.querySelector('.detailed_description').innerHTML += game.about_the_game || "No disponible";
+        contenedor.querySelector('.min-pc_requirements').innerHTML += game.pc_requirements.minimum || "No disponible";
+        contenedor.querySelector('.recomended-pc_requirements').innerHTML += game.pc_requirements.recommended || "No disponible";
+
 
     }
 }
